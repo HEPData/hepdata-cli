b7bc845b9aed70e45e87eb23e08c0394866a3048
GDeLaurentis <g.dl@hotmail.it>
2020-05-19 17:13:13 +0100

    Added download command with csv, root, yaml, and yoda options. Downloads all search matches in all specified file types.

--- hepdata_cli/api.py
+++ hepdata_cli/api.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import re
 
 
 class Client(object):
@@ -13,9 +14,50 @@ class Client(object):
         data = response.json()
         return data['results']
 
+    def download(self, query, csv=False, root=False, yaml=False, yoda=False):
+        results = self.find(query)
+        for result in results:
+            links = result['access_urls']['links']
+            if csv is True:
+                download_url(links['csv'])
+            if root is True:
+                download_url(links['root'])
+            if yaml is True:
+                download_url(links['yaml'])
+            if yoda is True:
+                download_url(links['yoda'])
+
     def _query(self, query):
         url = 'https://www.hepdata.net/search/?q=' + query + "&format=json"
         response = requests.get(url)
         if self.verbose is True:
             print("\nLooking up: " + url + "\n")
         return response
+
+
+def download_url(url):
+    assert is_downloadable(url), "Given url is not downloadable: {}".format(url)
+    response = requests.get(url, allow_redirects=True)
+    filename = getFilename_fromCd(response.headers.get('content-disposition'))
+    open(filename, 'wb').write(response.content)
+
+
+def getFilename_fromCd(cd):
+    """Get filename from content-disposition."""
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
+
+
+def is_downloadable(url):
+    """Does the url contain a downloadable resource?"""
+    header = requests.head(url, allow_redirects=True).headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
--- hepdata_cli/cli.py
+++ hepdata_cli/cli.py
@@ -17,3 +17,14 @@ def cli(ctx, verbose=False):
 @click.pass_obj
 def find(client, query):
     click.echo(client.find(query))
+
+
+@cli.command()
+@click.argument('query')
+@click.option('--csv', is_flag=True, help="Download type.")
+@click.option('--root', is_flag=True, help="Download type.")
+@click.option('--yaml', is_flag=True, help="Download type.")
+@click.option('--yoda', is_flag=True, help="Download type.")
+@click.pass_obj
+def download(client, query, csv=False, root=False, yaml=False, yoda=False):
+    client.download(query, csv=csv, root=root, yaml=yaml, yoda=yoda)
