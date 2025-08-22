a276c3f7d2dd245c23279ab910a1aee626924d0f
Graeme Watt <Graeme.Watt@durham.ac.uk>
2025-08-22 11:57:47 +0100

    ci: remove Python 3.6 and add 3.12/3.13
    * Also upgrade actions/checkout and actions/setup-python.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=install_requirements,
     tests_require=test_requirements,
     extras_require=extras_require,
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
             'hepdata-cli = hepdata_cli.cli:cli',
